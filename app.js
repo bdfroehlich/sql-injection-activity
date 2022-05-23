456b3f9270017e504b100d230f3ca7f14dfb977c
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-05-23 15:13:43 -0500

    updating styling

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.post('/login', function (req, res) {
 	var password = req.body.password;
 	var query = "SELECT title FROM user where username = '" + username + "' and password = '" + password + "'";
 
-	console.log("username: " + username);
-	console.log("password: " + password);
+	console.log("user: " + username);
+	console.log("pass: " + password);
 	console.log('query: ' + query);
 
 	db.get(query, function (err, row) {
@@ -40,7 +40,7 @@ app.post('/login', function (req, res) {
 		} else if (!row) {
 			res.redirect("/index.html#unauthorized");
 		} else {
-			res.send('Hello <b>' + row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>');
+			res.send('Hello <b>' + row.title + '!</b><br /> This file contains your data: <br /><br /> List of items: <br /><br /> Item #1 <br /><br /> Item #2 <br /><br /> <a href="/index.html">Login Page</a>');
 		}
 	});
 
